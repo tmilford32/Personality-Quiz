6b660697358d490f2745dd34195646de69d868a5
Tmilford <tmilford32@icloud.com>
2020-09-30 19:02:34 -0500

    My first commit

---
+++ Personality Quiz/IntroductionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by MXC Swift on 9/29/20.
+//  Copyright © 2020 MXC Swift. All rights reserved.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Personality Quiz/QuestionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Question:ViewController.swift
+//  Personality Quiz
+//
+//  Created by MXC Swift on 9/30/20.
+//  Copyright © 2020 MXC Swift. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
