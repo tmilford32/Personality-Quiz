3c3e649c78a91db8bc512cbcca08f399643adfe1
Tmilford <tmilford32@icloud.com>
2020-10-06 08:17:25 -0500

    Final build with bug

--- Personality Quiz/QuestionViewController.swift
+++ Personality Quiz/QuestionViewController.swift
@@ -16,7 +16,6 @@ class QuestionViewController: UIViewController {
     
     @IBOutlet var singleStackView: UIStackView!
     @IBOutlet var singleButton1: UIButton!
-    
     @IBOutlet var singleButton2: UIButton!
     
     @IBOutlet var singleButton3: UIButton!
@@ -31,10 +30,18 @@ class QuestionViewController: UIViewController {
     @IBOutlet var multiLabel4: UILabel!
     
     
+    @IBOutlet var multiSwitch1: UISwitch!
+    @IBOutlet var multiSwitch2: UISwitch!
+    @IBOutlet var multiSwitch3: UISwitch!
+    @IBOutlet var multiSwitch4: UISwitch!
+    
     
     @IBOutlet var rangedStackView: UIStackView!
     @IBOutlet var rangedLabel1: UILabel!
     @IBOutlet var rangedLabel2: UILabel!
+    @IBOutlet var rangedSlider: UISlider!
+    
+    
     
     @IBOutlet var questionProgressView: UIProgressView!
     
@@ -69,6 +76,10 @@ class QuestionViewController: UIViewController {
     ]
     
     var questionIndex = 0
+    
+    var answersChosen: [Answer] = []
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -115,11 +126,87 @@ class QuestionViewController: UIViewController {
     }
     
     func updateMultipleStack(using answers: [Answer]){
-         multipleStackView.isHidden = false
+        multipleStackView.isHidden = false
+        multiSwitch1.isOn = false
+        multiSwitch2.isOn = false
+        multiSwitch3.isOn = false
+        multiSwitch4.isOn = false
+        
+        multiLabel1.text = answers[0].text
+        multiLabel2.text = answers[1].text
+        multiLabel3.text = answers[2].text
+        multiLabel4.text = answers[3].text
+        
     }
     
     func updateRangedStack(using answers: [Answer]){
-         rangedStackView.isHidden = false
+        rangedStackView.isHidden = false
+        rangedSlider.setValue(0.5, animated: false)
+        rangedLabel1.text = answers.first?.text
+        rangedLabel2.text = answers.last?.text
+    }
+    
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
+        
+        switch sender {
+        case singleButton1:
+            answersChosen.append(currentAnswers[0])
+        case singleButton2:
+            answersChosen.append(currentAnswers[1])
+        case singleButton3:
+            answersChosen.append(currentAnswers[2])
+        case singleButton4:
+            answersChosen.append(currentAnswers[3])
+        default:
+            break
+        }
+      nextQuestion()
+    }
+    
+    @IBAction func multipleAnswerButtonPressed() {
+        let currentAnswers = questions[questionIndex].answers
+        
+        if multiSwitch1.isOn {
+            answersChosen.append(currentAnswers[0])
+        }
+        if multiSwitch2.isOn {
+        answersChosen.append(currentAnswers[1])
+        }
+        if multiSwitch3.isOn {
+            answersChosen.append(currentAnswers[2])
+        }
+        if multiSwitch4.isOn {
+        answersChosen.append(currentAnswers[3])
+        }
+        
+        nextQuestion()
+    }
+    
+    @IBAction func rangedAnswerButtonPressed() {
+        
+     let currentAnswers = questions[questionIndex].answers
+     
+     let index = Int(round(rangedSlider.value * Float(currentAnswers.count - 1)))
+     
+        answersChosen.append(currentAnswers[index])
+    }
+    
+    func nextQuestion(){
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            updateUI()
+        } else {
+            performSegue(withIdentifier: "ResultsSegue", sender: nil)
+        }
+    }
+        override func prepare (for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "ResultsSegue" {
+            let resultsViewController = segue.destination as! ResultsViewController
+            resultsViewController.responses = answersChosen
+            }
+        }
     }
     
     /*
@@ -132,4 +219,4 @@ class QuestionViewController: UIViewController {
     }
     */
 
-}
+
--- Personality Quiz/ResultsViewController.swift
+++ Personality Quiz/ResultsViewController.swift
@@ -10,13 +10,40 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
-    var myint = 12
+    @IBOutlet var resultAnswerLabel: UILabel!
+    @IBOutlet var resultDefinitionLabel: UILabel!
+    
+    
+    var responses: [Answer]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        calculatePersonalityResult()
     }
     
+    func calculatePersonalityResult() {
+        var frequencyOfAnswers: [CityType: Int] = [:]
+        let responseTypes = responses.map { $0.type }
+        
+        for response in responseTypes {
+            frequencyOfAnswers[response] = (frequencyOfAnswers[response] ?? 0) + 1
+        }
+        let frequentAnswersSorted = frequencyOfAnswers.sorted(by:
+        {(pair1, pair2) -> Bool in
+            return pair1.value > pair2.value
+        })
+    
+        let mostCommonAnswer = frequentAnswersSorted.first!.key
+        
+        resultAnswerLabel.text = "Your favorit City is\(mostCommonAnswer.rawValue)!"
+        
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+        
+        
+    }
 
     /*
     // MARK: - Navigation
